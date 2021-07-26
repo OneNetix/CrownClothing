14a52f8936434ba22f90f6ce6f6a56ecb64386d3
Liam Miller <liamcharlesmiller@gmail.com>
2021-07-26 13:06:19 +0100

    Updated app with shop page
    Added Collection preview component and collection item component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import "./App.css";
+import { Route, Link } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Route exact path="/" component={HomePage} />
+      <Route exact path="/shop" component={ShopPage} />
     </div>
   );
 }
